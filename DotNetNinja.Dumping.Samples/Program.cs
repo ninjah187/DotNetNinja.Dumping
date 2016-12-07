286f09c89468f71beac3b50aff4de6cefb0c6fce
karolhnz <karolhnz@gmail.com>
2016-12-07 18:45:00 +0100

    samples project initialized

---
+++ DotNetNinja.Dumping.Samples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNetNinja.Dumping.Samples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
