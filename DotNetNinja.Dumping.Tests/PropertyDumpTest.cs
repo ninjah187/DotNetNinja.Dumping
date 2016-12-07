bb41565c490ec4e28e4c63ba6b99ac984d5d7a8f
karolhnz <karolhnz@gmail.com>
2016-12-07 18:28:55 +0100

    PropertyDumpTest added

---
+++ DotNetNinja.Dumping.Tests/PropertyDumpTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNetNinja.Dumping.Tests
+{
+    public class PropertyDumpTest
+    {
+        string _testPropertyName = "testProperty";
+
+        [Fact]
+        public void ValueInConstructorIsNull_SetsValueString()
+        {
+            var dump = new PropertyDump(_testPropertyName, null);
+
+            Assert.Equal("[null]", dump.Value);
+        }
+
+        [Fact]
+        public void ValueInConstructorIsNotNull_SetsToStringedValue()
+        {
+            int value = 1;
+
+            var dump = new PropertyDump(_testPropertyName, value);
+
+            Assert.Equal("1", dump.Value);
+        }
+    }
+}
